993afaa8fc55d791dde6f3292912c3081cfa1876
Mihail Zdravkov <mihail0zdravkov@gmail.com>
2015-01-18 18:57:15 +0200

    Rotating matrices by 45 and 90 degrees

--- generation.go
+++ generation.go
@@ -66,8 +66,8 @@ func generateNewRegions(lines []int) []*[]Region {
 // (given that both have the same size) to produce a transformation in the relief.
 func randomElevationTransformation(subdivisions int) [][]int {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	width := r.Intn(int(math.Pow(4, float64(subdivisions)-1)))
-	height := r.Intn(int(math.Pow(4, float64(subdivisions)-1)))
+	width := r.Intn(int(math.Pow(4, float64(subdivisions)-1))) + 1
+	height := r.Intn(int(math.Pow(4, float64(subdivisions)-1))) + 1
 
 	matrix := make([][]int, height)
 	for i := 0; i < height; i++ {
@@ -100,4 +100,60 @@ func randomElevationTransformation(subdivisions int) [][]int {
 	}
 
 	return matrix
+}
+
+// func applyElevationTransformation(map *[]*[]Region, transformation *[][]int) {
+// 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+// 	x := r.Intn(int(20*math.Pow(4, float64(subdivisions)-1)))
+// 	height := r.Intn(int(math.Pow(4, float64(subdivisions)-1)))
+// }
+
+func rotateMatrix45(matrix [][]int) [][]int {
+	height := len(matrix)
+	width := len(matrix[0])
+
+	size := height + width - 1
+
+	result := make([][]int, size)
+	for i := 0; i < size; i++ {
+		row := make([]int, size)
+		result[i] = row
+	}
+
+	// the actual rotation of the matrix
+	for i := 0; i < height; i++ {
+		for k := 0; k < width; k++ {
+			result[k+i][k+height-1-i] = matrix[i][k]
+		}
+	}
+
+	// after rotating, between the diagonal lines of numbers remains 0s,
+	// so we assign to them the value of a neighbour
+	// (otherwise 0 can be found next to a big number and the terrain will be too hackly)
+	for i := 0; i < height-1; i++ {
+		for k := 0; k < width-1; k++ {
+			result[k+i+1][k+height-1-i] = result[k+i+1][k+height-2-i]
+		}
+	}
+
+	return result
+}
+
+func rotateMatrix90(matrix [][]int) [][]int {
+	height := len(matrix)
+	width := len(matrix[0])
+
+	result := make([][]int, width)
+	for i := 0; i < width; i++ {
+		column := make([]int, height)
+		result[i] = column
+	}
+
+	for i := 0; i < height; i++ {
+		for k := 0; k < width; k++ {
+			result[k][height-1-i] = matrix[i][k]
+		}
+	}
+
+	return result
 }
--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 package main
 
-//import "fmt"
+// import "fmt"
 
-func main() {
-}
+func main() {}
