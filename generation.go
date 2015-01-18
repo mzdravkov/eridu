719aa087131a2db5a080f05fb3b55a5c3a758a89
Mihail Zdravkov <mihail0zdravkov@gmail.com>
2015-01-18 16:13:51 +0200

    functions generateNewRegions and randomElevationTransformation

--- generation.go
+++ generation.go
@@ -2,30 +2,31 @@ package main
 
 import (
 	"math"
+	"math/rand"
+	"time"
 )
 
 // Returns how much triangles should have each line of a icoshpere
 // (sphere made from recursive subdivisioning of the triangles of an icosahedron).
 // The result is a slice where the index is the index of the line and the value
 // the number of triangles in that line.
-
 func trianglesInLines(s int) []int {
 	segmentSize := int(math.Pow(2, float64(s-1)))
 	result := make([]int, 3*segmentSize)
-	
+
 	triangles := 5
 	for i := 0; i < segmentSize; i++ {
 		result[i] = triangles
 		triangles += 10
 	}
 
-	triangles = 10*segmentSize
+	triangles = 10 * segmentSize
 	for i := segmentSize; i < 2*segmentSize; i++ {
 		result[i] = triangles
 	}
 
 	triangles = 10*segmentSize - 5
-	for i := 2*segmentSize; i < 3*segmentSize; i++ {
+	for i := 2 * segmentSize; i < 3*segmentSize; i++ {
 		result[i] = triangles
 		triangles -= 10
 	}
@@ -33,11 +34,70 @@ func trianglesInLines(s int) []int {
 	return result
 }
 
-struct Region {
-	Type uint8
+type Region struct {
+	Type      uint8
 	Elevation int16
 }
 
-func generateNewRegions(lines []int) error {
-	
+// Returns the count of the subdivisions when given the trianglesInLines
+func subdivisionsFromLines(lines []int) int {
+	result := 2 * len(lines) / 3
+	return int(math.Log2(float64(result)))
+}
+
+// Returns slice of pointers to slices of Regions.
+// All regions will be with 0 type and 0 elevation.
+func generateNewRegions(lines []int) []*[]Region {
+	subdivisions := subdivisionsFromLines(lines)
+	result := make([]*[]Region, subdivisions)
+
+	for i := 0; i < subdivisions; i++ {
+		layer := make([]Region, int(20*math.Pow(4, float64(i))))
+		for k := 0; k < len(layer); k++ {
+			layer[k] = Region{0, 0}
+		}
+		result[i] = &layer
+	}
+
+	return result
+}
+
+// Returns a transformation matrix that can be added to a random submatrix of the field
+// (given that both have the same size) to produce a transformation in the relief.
+func randomElevationTransformation(subdivisions int) [][]int {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	width := r.Intn(int(math.Pow(4, float64(subdivisions)-1)))
+	height := r.Intn(int(math.Pow(4, float64(subdivisions)-1)))
+
+	matrix := make([][]int, height)
+	for i := 0; i < height; i++ {
+		row := make([]int, width)
+		matrix[i] = row
+	}
+
+	for row := 0; row < height; row++ {
+		for column := 0; column < width; column++ {
+			maxByPosition := int(math.Min(float64(row), float64(column))) + 1
+
+			maxByNeighbours := 0
+			if column != 0 && row != 0 {
+				maxByNeighbours = int(math.Min(float64(matrix[row-1][column]), float64(matrix[row][column-1]))) + 1
+			} else if column != 0 {
+				maxByNeighbours = matrix[row][column-1] + 1
+			} else if row != 0 {
+				maxByNeighbours = matrix[row-1][column] + 1
+			}
+
+			max := 0
+			if maxByNeighbours != 0 {
+				max = int(math.Min(float64(maxByNeighbours), float64(maxByPosition)))
+			} else {
+				max = maxByPosition
+			}
+
+			matrix[row][column] = r.Intn(max + 1)
+		}
+	}
+
+	return matrix
 }
--- generation_test.go
+++ generation_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestTrianglesInLines(t *testing.T) {
 	result := trianglesInLines(3)
-	expected := []int {5, 15, 25, 35, 40, 40, 40, 40, 35, 25, 15, 5} 
+	expected := []int{5, 15, 25, 35, 40, 40, 40, 40, 35, 25, 15, 5}
 
 	if len(result) != len(expected) {
 		t.Error("Test is failing for trianglesInLines with subdivisions = 3")
@@ -13,6 +13,35 @@ func TestTrianglesInLines(t *testing.T) {
 	for i := 0; i < len(result); i++ {
 		if result[i] != expected[i] {
 			t.Error("Test is failing for trianglesInLines with subdivisions = 3")
+		}
+	}
+}
+
+func TestSubdivisionsFromLines(t *testing.T) {
+	if subdivisionsFromLines(trianglesInLines(3)) != 3 {
+		t.Error("subdivisionsFromLines doesn't return the right result.")
+	}
+}
+
+func TestGenerateNewRegions(t *testing.T) {
+	layers := generateNewRegions(trianglesInLines(3))
+
+	if len(*layers[0]) != 20 {
+		t.Error("generateNewRegions doesn't work.")
+	}
+
+	if len(*layers[1]) != 80 {
+		t.Error("generateNewRegions doesn't work.")
+	}
+
+	if len(*layers[2]) != 320 {
+		t.Error("generateNewRegions doesn't work.")
+	}
+
+	newRegion := Region{0, 0}
+	for i := 0; i < len(*layers[0]); i++ {
+		if (*layers[0])[i] != newRegion {
+			t.Error("generateNewRegions doesn't work.")
 		}
 	}
 }
