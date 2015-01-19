7ca1238b0db2817c4ca64bd323d209d83a473e85
Mihail Zdravkov <mihail0zdravkov@gmail.com>
2015-01-19 03:24:38 +0200

    write to file in order to try render the terrain using blender

--- file.go
+++ file.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func write(planet *[]*[]Region) {
+	csvfile, err := os.Create("output.csv")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer csvfile.Close()
+
+	writer := csv.NewWriter(csvfile)
+	for i := 0; i < len(*planet); i++ {
+		row := make([]string, len(*(*planet)[i]))
+		for k := 0; k < len(*(*planet)[i]); k++ {
+			row[k] = strconv.Itoa(int((*(*planet)[i])[k].Elevation))
+		}
+		err := writer.Write(row)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+
+	}
+	writer.Flush()
+}
--- main.go
+++ main.go
@@ -2,4 +2,19 @@ package main
 
 // import "fmt"
 
-func main() {}
+func main() {
+	triLines := trianglesInLines(5)
+	planet := generateNewRegions(triLines)
+	transformPlanetRelief(&planet)
+
+	write(&planet)
+
+	// for i := 0; i < 1536; i++ {
+	// 	o := len(*(planet[i]))
+	// 	// fmt.Println((*planet[i])[o-1])
+	// 	for k := int16(0); k < (*planet[i])[o-1].Elevation; k++ {
+	// 		fmt.Print(".")
+	// 	}
+	// 	fmt.Println("")
+	// }
+}
